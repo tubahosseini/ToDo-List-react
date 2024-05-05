0d58240c70a12db3c767b8f82c57be28c7bedfa5
Tuba Hosseini <tubahosseini@gmail.com>
2024-05-05 15:31:03 +0330

    create background in Home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,15 @@
+import TaskItem from "../../components/home/taskItem/TaskItem";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <>
+      <div className="bg-[#2968eb] fixed w-1/2 h-[80%] top-[10%] left-1/4 rounded-2xl -rotate-6"></div>
+      <div className="bg-white fixed w-1/2 h-[80%] top-[10%] left-1/4 rounded-2xl p-20">
+        <h1 className="font-bold text-2xl">ToDo List</h1>
+        <div className="px-4 py-2">
+          <TaskItem />
+        </div>
+      </div>
+    </>
+  );
 }
